bb93e45201ef054b70d71ba1f20c7d05598a1789
szorfein <szorfein@protonmail.com>
2020-12-23 20:51:33 +0100

    correct install on debian szorfein/dots#6

--- awesomewm/.config/awesome/layouts/monitor_bar/init.lua
+++ awesomewm/.config/awesome/layouts/monitor_bar/init.lua
@@ -5,6 +5,8 @@ return function(s)
     require("layouts.monitor_bar.horizontal")(s)
   elseif M.name == "worker" then
     require("layouts.monitor_bar.horizontal_v2")(s)
+  elseif M.name == "astronaut" then
+    require("layouts.monitor_bar.astronaut")(s)
   else
     require("layouts.monitor_bar.horizontal")(s)
     s.monitor_bar.visible = false
