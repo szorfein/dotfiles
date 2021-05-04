ddbe8c84813eba9edc01bc8567baec50982ced7c
szorfein <szorfein@protonmail.com>
2021-05-04 15:41:06 +0200

    enhance little-gem.sh

--- bin/bin/little-gem.sh
+++ bin/bin/little-gem.sh
@@ -2,7 +2,7 @@
 
 # Script for initialize a Ruby project 
 # The project is added in your current directory
-#
+
 # Usage: little-gem.sh [-p NAME]
 # Examples:
 # little-gem.sh -p awesome
@@ -30,26 +30,36 @@ EOF
 }
 
 gen_gemspec() {
-    content="
-require File.dirname(__FILE__) + \"/lib/$PROJ_LOW/version\"
+    content="require File.dirname(__FILE__) + \"/lib/$PROJ_LOW/version\"
+
+# https://guides.rubygems.org/specification-reference/
 Gem::Specification.new do |s|
-  s.name = \"$PROJ_LOW\"
-  s.version = $PROJ_CAP::VERSION
-  s.summary = \"Awesome Ruby Project !\"
-  s.author = [\"$USERNAME\"]
-  s.email = [\"$EMAIL\"]
-  s.homepage = \"project url | github page\"
-  s.metadata = \{
-    \"changelog_uri\" => \"https://github.com/$USERNAME/$PROJ/blob/master/CHANGELOG.md\",
-    \"bug_tracker_uri\" => \"https://github.com/$USERNAME/$PROJ/issues\",
-    \"wiki_uri\" => \"https://github.com/$USERNAME/$PROJ/docs\",
-  \}
-  s.license = \"MIT\"
-  s.required_ruby_version = \">=$RUBY_VERSION_REQUIRED\"
   s.files = \`git ls-files\`.split(\" \")
   s.files.reject! { |fn| fn.include? \"certs\" }
-  s.executables = [ \"$PROJ_LOW\" ]
+  s.name = \"$PROJ_LOW\"
+  s.summary = \"Awesome Ruby Project !\"
+  s.version = $PROJ_CAP::VERSION
+  s.description = <<-EOF
+    $PROJ is just an awesome gem !
+  EOF
+  s.email = \"$EMAIL\"
+  s.homepage = \"https://github.com/$USERNAME/$PROJ\"
+  s.license = \"MIT\"
+  s.metadata = {
+    \"bug_tracker_uri\" => \"https://github.com/$USERNAME/$PROJ/issues\",
+    \"changelog_uri\" => \"https://github.com/$USERNAME/$PROJ/blob/master/CHANGELOG.md\",
+    \"source_code_uri\" => \"https://github.com/$USERNAME/$PROJ\",
+    \"wiki_uri\" => \"https://github.com/$USERNAME/$PROJ/wiki\",
+    \"funding_uri\" => \"https://patreon.com/$USERNAME\",
+  }
+  s.author = \"$USERNAME\"
+  s.bindir = \"bin\"
   s.cert_chain = [\"certs/$USERNAME.pem\"]
+  s.executables << \"$PROJ_LOW\"
+  s.extra_rdoc_files = ['README.md']
+  s.required_ruby_version = \">=$RUBY_VERSION_REQUIRED\"
+  s.requirements << 'TODO change: libmagick, v6.0'
+  s.requirements << 'TODO change: A good graphics card'
   s.signing_key = File.expand_path(\"~/.ssh/gem-private_key.pem\") if \$@ =~ /gem\z/
 end
 "
@@ -57,8 +67,7 @@ end
 }
 
 gen_lib() {
-    content="
-require_relative '$PROJ_LOW/version'
+    content="require_relative '$PROJ_LOW/version'
 module $PROJ_CAP
 end
 "
@@ -74,15 +83,27 @@ gen_changelog() {
 }
 
 gen_readme() {
-    content="
-# $PROJ_CAP
+    content="# $PROJ_CAP
+Awesome ruby gem to build: $PROJ !
+
+## Gem build
+
+    gem build $PROJ.gemspec
+
+## Gem push
+
+    gem login
+    gem push $PROJ-0.0.1.gem
+
+## Install $PROJ locally
+
+    gem install $PROJ-0.0.1.gem -P HighSecurity
 "
     cat_file "$content" "README.md"
 }
 
 gen_version() {
-    content="
-module $PROJ_CAP
+    content="module $PROJ_CAP
   VERSION = '0.0.1'.freeze
 end
 "
@@ -90,17 +111,56 @@ end
 }
 
 gen_bin() {
-    content="
-#!/usr/bin/env ruby
-require '$PROJ_LOW'
-print "$PROJ_CAP::VERSION"
+    content="#!/usr/bin/env ruby
+require \"$PROJ_LOW\"
+puts \"$PROJ_CAP v.\" + $PROJ_CAP::VERSION
 "
     cat_file "$content" "bin/$PROJ_LOW"
-    chmod 700 "$PROJ/bin/$PROJ_LOW"
+    chmod 744 "$PROJ/bin/$PROJ_LOW"
+}
+
+gen_MIT_license() {
+    content="MIT License
+
+Copyright (c) $(date '+%Y') $USERNAME
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"
+    cat_file "$content" LICENSE
+
+}
+
+copy_pubkey() {
+    PUBKEY="$HOME/.ssh/gem-public_cert.pem"
+    [ -f "$PUBKEY" ] || {
+        echo "[-] No public key found."
+        return
+    }
+    cp -v "$PUBKEY" "$PROJ/certs/$USERNAME.pem"
+}
+
+git_init() {
+    if ! hash git 2>/dev/null ; then
+        echo "[-] git was no found"
+        return
+    fi
+    (
+        cd "$PROJ" \
+            && git init \
+            && git add . \
+            && git commit -a -S -m "initial commit" \
+            && git remote add origin "https://github.com/$USERNAME/$PROJ.git"
+    )
+    echo "===> Git initialized, you can push this gem with:"
+    echo "cd $PROJ && git push -u origin master"
 }
 
 create_project() {
-    echo "===> Create project $PROJ"
+    echo "===> Building your gem project $PROJ"
     mkdir -p "$PROJ"/{bin,lib,certs}
     mkdir -p "$PROJ"/lib/"$PROJ_LOW"
     gen_changelog
@@ -109,6 +169,9 @@ create_project() {
     gen_lib
     gen_version
     gen_bin
+    gen_MIT_license
+    copy_pubkey
+    git_init
 }
 
 make_vars() {
@@ -124,10 +187,10 @@ main() {
     [ -d "$PROJ" ] && die "$PROJ exist alrealy..."
     make_vars
     create_project
-    echo "You can try your gem with:"
+    echo "===> You can try your gem with:"
     echo "cd $PROJ"
     echo "ruby -I lib bin/$PROJ_LOW"
-    echo "cyaa"
+    printf "\n+cyaa+\n"
     exit 0
 }
 
