15e5f70e29d8ded5e3419d17667f6a2ea3247633
szorfein <szorfein@protonmail.com>
2022-03-03 21:56:01 -0500

    base layout.panel-music

---
+++ awm-m3/.config/awesome/layout/panel-music.lua
@@ -0,0 +1,142 @@
+local wibox = require('wibox')
+local helpers = require('lib.helpers')
+
+local music = class()
+
+function music:init()
+  self.title = self:title()
+  self.image = self:image()
+  self.progressbar = self:progressbar(33)
+  return wibox.widget {
+    {
+      self:top(),
+      self:middle(),
+      self:bottom(),
+      expand = 'none',
+      layout = wibox.layout.align.vertical
+    },
+    left = dpi(12), right = dpi(12),
+    widget = wibox.container.margin
+  }
+end
+
+function music:image()
+  return wibox.widget {
+    image = '/home/daggoth/images/thumb-1920-609120.jpg',
+    widget = wibox.widget.imagebox
+  }
+end
+
+function music:progressbar(value)
+  return wibox.widget {
+    max_value     = 1,
+    value         = value,
+    max_value     = 100,
+    forced_height = 10,
+    forced_width  = 100,
+    paddings      = 1,
+    color = md.sys.color.secondary,
+    background_color = md.sys.color.secondary
+      .. md.sys.elevation.level1,
+    shape = helpers.rrect(dpi(20)),
+    widget        = wibox.widget.progressbar,
+  }
+end
+
+function music:top()
+  return wibox.widget {
+    {
+      text = 'MPD status:',
+      widget = wibox.widget.textbox
+    },
+    layout = wibox.layout.fixed.vertical
+  }
+end
+
+function music:title()
+  return wibox.widget {
+    {
+      {
+        text = '> TITLE',
+        align = 'center',
+        font = md.sys.typescale.body_medium.font
+          .. ' ' .. md.sys.typescale.body_medium.size,
+        widget = wibox.widget.textbox
+      },
+      {
+        text = 'by AUTHOR',
+        align = 'center',
+        font = md.sys.typescale.body_small.font
+          .. ' ' .. md.sys.typescale.body_small.size,
+        widget = wibox.widget.textbox
+      },
+      widget = wibox.layout.fixed.vertical
+    },
+    top = dpi(20),
+    widget = wibox.container.margin
+  }
+end
+
+function music:middle()
+  return wibox.widget {
+    {
+      self.title,
+      {
+        self.image,
+        {
+          self.progressbar,
+          top = dpi(20),
+          left = dpi(10), right = dpi(10),
+          widget = wibox.container.margin
+        },
+        layout = wibox.layout.fixed.vertical
+      },
+      {
+        nil,
+        {
+          {
+            {
+              text = '󰼨',
+              font = md.sys.typescale.icon.font .. ' ' .. dpi(20),
+              widget = wibox.widget.textbox
+            },
+            {
+              text = '󰼛',
+              font = md.sys.typescale.icon.font .. ' ' .. dpi(20),
+              widget  = wibox.widget.textbox
+            },
+            {
+              text = '󰼧',
+              font = md.sys.typescale.icon.font .. ' ' .. dpi(20),
+              widget = wibox.widget.textbox
+            },
+            spacing = dpi(8),
+            layout = wibox.layout.fixed.horizontal
+          },
+          bottom = dpi(10),
+          widget = wibox.container.margin
+        },
+        expand = 'none',
+        layout = wibox.layout.align.horizontal
+      },
+      expand = 'none',
+      layout = wibox.layout.align.vertical
+    },
+    bg = md.sys.color.primary .. md.sys.elevation.level1,
+    forced_height = dpi(400),
+    shape = helpers.rrect(dpi(28)),
+    widget = wibox.container.background
+  }
+end
+
+function music:bottom()
+  return wibox.widget {
+    {
+      text = 'Playlists',
+      widget = wibox.widget.textbox
+    },
+    layout = wibox.layout.fixed.vertical
+  }
+end
+
+return music
--- awm-m3/.config/awesome/layout/panel.lua
+++ awm-m3/.config/awesome/layout/panel.lua
@@ -38,15 +38,51 @@ function panel:wibox_args()
   }
 end
 
-function panel:setup()
-  self.screen.panel:setup {
-    text_button({
+function panel:head_buttons()
+  return wibox.widget {
+    {
+      text_button({
+        content = 'm',
+        cmd = function()
+          self:music_panel()
+        end
+      }),
+      text_button({
+        content = 'c',
+        cmd = function()
+          self:control_panel()
+        end
+      }),
+      layout = wibox.layout.fixed.horizontal
+    },
+    nil, -- center
+    text_button({ -- right
       content = 'ﰸ',
       cmd = function() self:hide() end,
     }),
+    layout = wibox.layout.align.horizontal
+  }
+end
+
+function panel:control_panel()
+  self.screen.panel:setup {
+    self:head_buttons(),
     require('layout.panel-control')(),
     layout = wibox.layout.align.vertical
   }
+end
+
+function panel:music_panel()
+  self.screen.panel:setup {
+    self:head_buttons(),
+    require('layout.panel-music')(),
+    layout = wibox.layout.align.vertical
+  }
+end
+
+-- default panel
+function panel:setup()
+  self:music_panel()
 end
 
 return panel
